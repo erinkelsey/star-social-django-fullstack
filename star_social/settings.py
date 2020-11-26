bc548b5b85732438679796654606ef0e0b1bca78
erinkelsey <erinszilagyi@gmail.com>
2020-11-26 01:43:27 -0600

    Add STATIC_ROOT to settings

--- star_social/settings.py
+++ star_social/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
